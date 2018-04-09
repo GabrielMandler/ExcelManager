cfca7a7f265ebfba90588c8e4fd4beaf3ce6bc06
Gabriel <gabrimandler@gmail.com>
2018-04-09 11:37:48 +0300

    files updated

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ OUTPUT_SHEET_DIRECTORY = r"C:\Users\gabriel\Documents\new_src.xlsx"
 ################################
 
 def main():
-    """txt_file_1, txt_file_2, output_file_directory = merge_directory_names(PARTICIPANT_NAME)
+    txt_file_1, txt_file_2, output_file_directory = merge_directory_names(PARTICIPANT_NAME)
     
     excel_file_1_directory, excel_file_2_directory = convert_input_files_to_txt_file(txt_file_1, txt_file_2)    
     
@@ -21,10 +21,7 @@ def main():
     new_matrix = merge_trial_indices_lines(output_file_directory)
     
     create_new_result_file(output_file_directory, new_matrix)
-    """
-    new_matrix = create_new_duplicated_row_matrix(SHEET_DIRECTORY)
-    
-    create_new_result_file(OUTPUT_SHEET_DIRECTORY, new_matrix)
+
     
     
 if __name__ == '__main__':main()
